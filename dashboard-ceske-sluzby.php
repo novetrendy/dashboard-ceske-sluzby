dc2044adfdf9a121c429c5d5565f6903f0d0cb65
novetrendy <oveckovaalena@novetrendy.eu>
2017-01-13 08:19:58 +0100

    Added settings button

--- dashboard-ceske-sluzby.php
+++ dashboard-ceske-sluzby.php
@@ -3,12 +3,15 @@
 * Plugin Name: Adresy feedů pro České služby
 * Plugin URI: https://webstudionovetrendy.eu/
 * Description: Zobrazí na nástěnce widget s adresami aktivovaných feedů. Pro chod pluginu jsou nutné pluginy České služby a WooCommerce
-* Version: 170112
+* Version: 170113
 * Author: Webstudio Nove Trendy
 * Author URI: https://webstudionovetrendy.eu/
 * GitHub Plugin URI: https://github.com/novetrendy/dashboard-ceske-sluzby
 * License: GPL2
 *** Changelog ***
+2017.01.13 - version 170113
+* Added settings button
+/*
 2017.01.12 - version 170112
 * Added date and time of last update feed
 /*
@@ -41,7 +44,26 @@ function ceske_sluzby_feedy_dashboard_widget() {
         //print_r(get_option( 'active_plugins'));
         ?>
         <div style="width:100%">
+            <p style="font-size:18px;line-height:22px;font-weight:bold;margin-bottom:15px;">Nastavení</p>
+            <form  method="post">
+        <a href="admin.php?page=wc-settings&tab=ceske-sluzby"><input style="margin-bottom:5px;"  class="button-primary woocommerce-save-button" type="submit" name="nastaveni" value="Obecné"></a>
+        <?php
+        if (get_option( 'wc_ceske_sluzby_heureka_xml_feed-aktivace' ) == 'yes'){ ?>
+        <a href="admin.php?page=wc-settings&tab=ceske-sluzby&section=xml-feed"><input style="margin-bottom:5px;"  class="button-primary woocommerce-save-button" type="submit" name="nastavenixml" value="XML"></a>
+        <?php
+        }
+        if ( get_option( 'wc_ceske_sluzby_dalsi_nastaveni_dodaci_doba-aktivace' ) == "yes" ) { ?>
+        <a href="admin.php?page=wc-settings&tab=ceske-sluzby&section=dodaci-doba"><input style="margin-bottom:5px;" class="button-primary woocommerce-save-button" type="submit" name="nastavenidd" value="Dodací doba"></a>
+        <?php
+        if (get_option( 'wc_ceske_sluzby_heureka_certifikat_spokojenosti-aktivace' ) == 'yes'){ ?>
+        <a href="admin.php?page=wc-settings&tab=ceske-sluzby&section=certifikat-spokojenosti"><input style="margin-bottom:5px;"  class="button-primary woocommerce-save-button" type="submit" name="certifikat" value="Certifikát spokojenosti"></a>
+        <?php
+        }
+        ?>
+    </form>
             <?php
+            }
+        echo '<hr>';
             if (get_option( 'wc_ceske_sluzby_xml_feed_heureka-aktivace') == 'yes'){
             ?>
             <div style="width:45%;float:left;">
